8471513ead2f6009d81952b5e66397b5b412a7f7
zanettiprado <zanettiprado@gmail.com>
2023-04-10 14:47:12 +0000

    saving

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,8 +15,12 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
-
+/**
+ * The main game "loop", called when the script is first loaded
+ * and afterthe user answer has been processed
+ */
 function runGame() {
+    // creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() *25) +1;
     let num2 = Math.floor(Math.random() *25) +1;
 
