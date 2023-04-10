13e8fe0a133f4e925ac90dea3d2d6b62fe9fd3bf
zanettiprado <zanettiprado@gmail.com>
2023-04-10 13:59:28 +0000

    set up structure of the game

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer () {
+
+}
+
+function calculateCorrectAnwser() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion () {
+
+}
+
+function displaySubtractQuestion () {
+
+}
+
+function displayMultiplayQuestions () {
+    
+}
