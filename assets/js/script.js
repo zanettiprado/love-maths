699eaf7ed3d0aaca3c8cf619fbd71db484ecedda
zanettiprado <zanettiprado@gmail.com>
2023-04-11 23:22:41 +0000

    add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,8 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplayQuestions(num1, num2);
     } else {
         alert(`Unknow game type: ${gameType}`);
         throw `Unknow game type: ${gameType}. Aborting!`;
@@ -65,7 +67,9 @@ function calculateCorrectAnwser() {
     let operator = document.getElementById("operator").innerText;
 
     if (operator === "+") {
-        return[operand1 + operand2, "addition"];
+        return [operand1 + operand2, "addition"];
+    } else if(operator === "x") {
+        return [operand1 * operand2, "multiply"]
     } else { 
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`; 
@@ -103,6 +107,10 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplayQuestions() {
+function displayMultiplayQuestions(operand1, operand2) {
+
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
 
 }
