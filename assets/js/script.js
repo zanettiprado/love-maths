1768d0c9e114b3f9de43b9c921f688e1d0bdf15f
zanettiprado <zanettiprado@gmail.com>
2023-04-11 21:16:50 +0000

    display addition questions to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,19 +10,28 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You clicked submit!"); 
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType)
             }
         });
     }
-});
+
+    runGame("addition");
+})
 /**
  * The main game "loop", called when the script is first loaded
  * and afterthe user answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
     // creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() *25) +1;
     let num2 = Math.floor(Math.random() *25) +1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknow game type: ${gameType}`);
+        throw `Unknow game type: ${gameType}. Aborting!`;
+    }
 
 }
 
@@ -42,7 +51,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion () {
+function displayAdditionQuestion (operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 
 }
 
