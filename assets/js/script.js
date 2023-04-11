1139975c9707352fb0398418c2e934ef2e3419de
zanettiprado <zanettiprado@gmail.com>
2023-04-11 23:04:40 +0000

    add code to increment score and incorrect tally answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,8 +46,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Awww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
     
     runGame(calculatedAnswer[1])
@@ -70,12 +72,23 @@ function calculateCorrectAnwser() {
     }
 
 }
-
+/**
+ * Gets the current score from the DOM and increments it by 1. 
+ */
 function incrementScore() {
 
-}
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
+
+}
+/**
+ * Gets the current tally of incorret answers from the DOM and increment it by 1. 
+ */
 function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
